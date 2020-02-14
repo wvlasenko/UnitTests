c1a28259a6d1eb3b4b3cd4c0f180ca757b0d3d66
Volodymyr Vlasenko <wvlasenko@gmail.com>
2020-02-14 16:41:09 +0200

    Add commit

--- src/test/java/ua/unit/testa/MatchTest.java
+++ src/test/java/ua/unit/testa/MatchTest.java
@@ -2,23 +2,23 @@ package ua.unit.testa;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MatchTest {
-    private Match  match = new Match();
+    private Match match = new Match();
 
-
+    //todo
     @Test
     void add() {
         int expected = 3;
-        int actual = match.add(1,2);
-        assertEquals(expected,actual);
+        int actual = match.add(1, 2);
+        assertEquals(expected, actual);
     }
 
     @Test
     void subtract() {
         int expected = 8;
-        int actual = match.subtract(20,12);
-        assertEquals(expected,actual);
+        int actual = match.subtract(20, 12);
+        assertEquals(expected, actual);
     }
 }
